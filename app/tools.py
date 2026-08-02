fef3a2a4ff14ee96c82601019bc0bf67179a35a4
YOGESH PARTE <yogeshparte@gmail.com>
2026-08-02 16:53:51 -0400

    Add ADK FunctionTool registration placeholder

---
+++ app/tools.py
@@ -0,0 +1,9 @@
+"""ADK FunctionTool registrations (CLI wrappers)."""
+
+# Example pattern:
+# from google.adk.tools import FunctionTool
+# from src.agentic_layer.tools.ontogpt_cli import run_ontogpt
+#
+# ontogpt_tool = FunctionTool(run_ontogpt)
+
+TOOLS = []
